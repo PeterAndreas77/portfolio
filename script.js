a4310ea46776c4635ec7d80e5fdcf0cc52d6ba03
PeterAndreas77 <andreas.soedharto@gmail.com>
2018-10-25 09:24:17 -0700

    added scripts

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-document.addEventListener(
-  "Checkbox shift content",
-  function() {
-    let checkbox = document.getElementById("menu");
-    let content = document.getElementsByClassName("content");
-    if (checkbox.checked) {
-      content.style.marginTop = "4rem";
-      console.log("checked");
-    }
-  },
-  false
-);
+"use strict";
+
+$(document).ready(() => {});
