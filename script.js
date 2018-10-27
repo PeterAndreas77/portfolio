3e4734b30724e505c6ca65225fa860bd87f11a74
PeterAndreas77 <andreas.soedharto@gmail.com>
2018-10-27 11:22:14 -0700

    added query for >768

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(() => {
   let checked = $("#menu").prop("checked");
   //Close navigation bar if user clicks outside
   $(".content").on("click", () => {
-    if (!checked) {
+    if (!checked && screen.width < 768) {
       $(".menu-content").css("max-height", 0);
     }
   });
