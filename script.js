27ddf051f1ea4340f02e1e546521195ef1e59e57
PeterAndreas77 <andreas.soedharto@gmail.com>
2018-10-27 09:53:30 -0700

    streamlined css

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 "use strict";
-
-$(document).ready(() => {});
+$(document).ready(() => {
+  let checked = $("#menu").prop("checked");
+  //Close navigation bar if user clicks outside
+  $(".content").on("click", () => {
+    if (!checked) {
+      $(".menu-content").css("max-height", 0);
+    }
+  });
+});
